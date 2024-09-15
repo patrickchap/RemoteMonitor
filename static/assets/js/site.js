69be99d6b2be86f5959ae9c4187b5b4272a2dec0
Patrick <patrick.chapple28@icloud.com>
2024-09-15 01:21:22 -0600

    fix js bug for modal where model code was running on every htmx trigger

--- static/assets/js/site.js
+++ static/assets/js/site.js
@@ -2,45 +2,49 @@
 console.log('site.js loaded');
 
 document.body.addEventListener('htmx:afterOnLoad', (evt) => {
-  const $targetEl = document.getElementById('crud-modal');
-  console.log($targetEl);
-
-  if ($targetEl) {
+  if (evt.detail.target.id === 'hostservices') {
     console.log('modal is loaded');
-    const options = {
-      placement: 'center',
-      backdrop: 'dynamic',
-      backdropClasses: 'bg-gray-900/50 dark:bg-gray-900/80 fixed inset-0 z-40',
-      closable: true,
-      onHide: () => {
-        console.log('modal is hidden');
-      },
-      onShow: () => {
-        console.log('modal is shown');
-      },
-      onToggle: () => {
-        console.log('modal has been toggled');
-      }
-    };
+
+    const $targetEl = document.getElementById('crud-modal');
+    console.log(evt.detail.target);
+
+    if ($targetEl) {
+      console.log('modal is loaded');
+      const options = {
+        placement: 'center',
+        backdrop: 'dynamic',
+        backdropClasses: 'bg-gray-900/50 dark:bg-gray-900/80 fixed inset-0 z-40',
+        closable: true,
+        onHide: () => {
+          console.log('modal is hidden');
+        },
+        onShow: () => {
+          console.log('modal is shown');
+        },
+        onToggle: () => {
+          console.log('modal has been toggled');
+        }
+      };
 
 
-    /*
-    * $targetEl: required
-    * options: optional
-    */
-    const modal = new Modal($targetEl, options);
+      /*
+      * $targetEl: required
+      * options: optional
+      */
+      const modal = new Modal($targetEl, options);
 
-    const addServiceButton = document.getElementById("add-service");
-    addServiceButton.addEventListener('click', () => {
-      modal.show();
-    });
+      const addServiceButton = document.getElementById("add-service");
+      addServiceButton.addEventListener('click', () => {
+        modal.show();
+      });
 
-    const closeButton = document.getElementById("close-model");
-    closeButton.addEventListener('click', () => {
-      modal.hide();
-    });
+      const closeButton = document.getElementById("close-model");
+      closeButton.addEventListener('click', () => {
+        modal.hide();
+      });
+    }
   }
-  // options with default values
+  // o}ptions with default values
 
 });
 
